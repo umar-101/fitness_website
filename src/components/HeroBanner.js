9c7181f49e6e08b69e34bb89b88eca23b149d6ef
UMAR <umarmushtaq101@gmail.com>
2022-06-21 22:52:52 +0500

    final--

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Stack, Typography, Button } from "@mui/material";
 
-import HeroBannerImage from "../assets/images/banner.png";
+import HeroBannerImage from "../assets/images/banner.jpg";
 const HeroBanner = () => {
   return (
     <Box
