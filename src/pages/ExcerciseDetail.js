a7ab1f74a380f30b0f04601f90d0b956c8572fdf
UMAR <umarmushtaq101@gmail.com>
2022-06-21 10:41:28 +0500

    exercise detail data loaded

--- src/pages/ExcerciseDetail.js
+++ src/pages/ExcerciseDetail.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExcerciseDetail = () => {
-  return <div>ExcerciseDetail</div>;
+  return (
+    <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExcerciseDetail;
